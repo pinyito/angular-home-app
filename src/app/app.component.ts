e039541c47f6e3f7672365d61e625559509e9e54
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2025-05-05 23:54:25 -0500

    Migrated project to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: true,
+    imports: [HomeComponent]
 })
 export class AppComponent {
   title = 'homes';
