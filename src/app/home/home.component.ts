fd9ac0eefd80caf8b78dbc0b09ab4ea87c11da40
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2025-05-06 11:15:24 -0500

    Added housing location interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
     selector: 'app-home',
@@ -13,5 +14,16 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
     ]
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  housingLocation: Housinglocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: '${this.baseUrl}/example-house.jpg',
+    availableUnits: 99,
+    wifi: true,
+    laundry: true,
+  };
 }
