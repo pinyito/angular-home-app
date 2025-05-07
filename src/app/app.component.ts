236e21da747e5b819186197b2eff624a51220ffe
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2025-05-06 22:49:12 -0500

    Added routes to specific components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     standalone: true,
-    imports: [HomeComponent]
+    imports: [
+      HomeComponent,
+      RouterModule
+    ]
 })
 export class AppComponent {
   title = 'homes';
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-
+import { provideRouter } from '@angular/router';
+import routeConfig from './app/routes';
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule)]
+    providers: [
+      importProvidersFrom(BrowserModule),
+      provideRouter(routeConfig)
+    ]
 })
   .catch(err => console.error(err));
